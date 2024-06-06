f323bc131b0f6745fef3be88416017b2ff78cd5a
Lorenzo Dalla Muta <lorenzodallamuta@gmail.com>
2024-06-06 14:57:47 +0200

    half assed attempt at vanilla app router i18n routing

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,20 @@
-import createNextIntlPlugin from "next-intl/plugin";
-
-const withNextIntl = createNextIntlPlugin();
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  i18n: {
+    locales: ["en", "es"],
+    defaultLocale: "en",
+    /* not sure how this would work with localhost */
+    // domains: [
+    //   {
+    //     domain: "local.com",
+    //     defaultLocale: "en",
+    //   },
+    //   {
+    //     domain: "local.es",
+    //     defaultLocale: "es",
+    //   },
+    // ],
+  },
+};
 
-export default withNextIntl(nextConfig);
+export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { NextIntlClientProvider } from "next-intl";
-import { getLocale, getMessages } from "next-intl/server";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,22 +11,14 @@ export const metadata: Metadata = {
 
 export default async function RootLayout({
   children,
-}: Readonly<{
+  params: { locale },
+}: {
   children: React.ReactNode;
-}>) {
-  const locale = await getLocale();
-
-  // Providing all messages to the client
-  // side is the easiest way to get started
-  const messages = await getMessages();
-
+  params: { locale: string };
+}) {
   return (
     <html lang={locale}>
-      <body className={inter.className}>
-        <NextIntlClientProvider messages={messages}>
-          {children}
-        </NextIntlClientProvider>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
